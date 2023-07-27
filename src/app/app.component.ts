53ccb90d23fc59aae96e9f06b75618babc12ed65
Suman Saurav -X (sumansa - SRS CONSULTING INC at Cisco) <sumansa@cisco.com>
2023-07-27 12:27:57 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  integerPattern = /^\d+$/;
+  emailRegx = "(^[_A-Za-z0-9-\+]+(\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\.[A-Za-z0-9]+)*(\.[A-Za-z]{2,})$";
   title = 'REACTIVEFORMS';
+
+
+  // this FormGroup constructor takes javascript object as its arguments
+
+  form = new FormGroup({
+    firstName: new FormControl("", Validators.required),
+    lastName: new FormControl(""),
+    age: new FormControl(""),
+    email: new FormControl(""),
+    mobile: new FormControl(""),
+    countries: new FormControl(),
+    gender: new FormControl()
+  });
+
+  // getControl(name: any) AbstractControl | null {
+  //   return this.form.get(name);
+  // }
+  submitFun() {
+    console.log(this.form.value);
+
+  }
+
+  getControl(name: any): AbstractControl | null {
+    return this.form.get('name');
+  }
+
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
   ],
+  // when we work with template driven form we need to import 'ReactiveFormsModule' which is imported from '@angular/forms';
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
